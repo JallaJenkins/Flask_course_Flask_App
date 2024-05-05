0c9953a04faa8aea0f231d36ab9df62afb071f53
JallaJenkins <wakeupalready@gmail.com>
2024-05-05 12:37:43 -0700

    "May 5 commit"

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
-from flask import Flask, jsonify, request, url_for, redirect, session, render_template
+from flask import Flask, jsonify, request, url_for, redirect, session, render_template, g
+import sqlite3
 
 app = Flask(__name__)
 
 # app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = "Thisisasecret"
 
+def connect_db():
+  sql = sqlite3.connect('data.db')
+  sql.row_factory = sqlite3.Row
+  return sql
+
+def get_db():
+  if not hasattr(g, 'sqlite3'):
+    g.sqlite_db = connect_db()
+  return g.sqlite_db
+
+@app.teardown_appcontext()
+def close_db(error):
+  if hasattr(g, 'sqlite_db'):
+    g.sqlite_db.close()
+
 @app.route("/")
 def index():
   session.pop('name', None)
   return f"<h1>Hello!</h1>"
 
-@app.route("/home", methods=['POST', 'GET'], defaults={'name' : "Default"})
+@app.route("/home/", methods=['POST', 'GET'], defaults={'name' : "Default"})
 @app.route("/home/<string:name>", methods=['POST', 'GET'])
 def home(name):
   session['name'] = name
